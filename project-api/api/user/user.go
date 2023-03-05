fe160f4106856e9b577010fc36977e3f8a5a734c
JeffyGu <JeffyGu@163.com>
2023-03-05 13:03:33 +0800

    导入redis支持

--- project-api/api/user/user.go
+++ project-api/api/user/user.go
@@ -1,34 +1,52 @@
 package login
 
 import (
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 	common "test.com/project-common"
+	"test.com/project-user/pkg/dao"
+	"test.com/project-user/pkg/model"
+	"test.com/project-user/pkg/repo"
 	"time"
 )
 
 type HandlerLogin struct {
+	cache repo.Cache
+}
+
+func New() *HandlerLogin {
+	return &HandlerLogin{
+		cache: dao.Rc,
+	}
 }
 
 // GetCaptcha 获取手机验证码
-func (HandlerLogin) GetCaptcha(ctx *gin.Context) {
+func (h *HandlerLogin) GetCaptcha(ctx *gin.Context) {
 	result := &common.Result{}
 	//1. 获取参数
 	mobile := ctx.PostForm("mobile")
 	//2. 验证手机合法性
 	if !common.VerifyMobile(mobile) {
-		ctx.JSON(200, result.Fail(2001, "不合法"))
+		ctx.JSON(http.StatusOK, result.Fail(model.NoLegalMobile, "不合法"))
 		return
 	}
 	//3.生成验证码
 	code := "123456"
-	//4. 发送验证码
+	//4. 调用短信平台发送验证码（放入go协程）
 	go func() {
 		time.Sleep(2 * time.Second)
 		log.Println("调用短信平台发送短信")
 		//发送成功 存入redis
 		fmt.Println(mobile, code)
+		c, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		err := h.cache.Put(c, "REGISTER_"+mobile, code, 15*time.Minute)
+		defer cancel()
+		if err != nil {
+			log.Printf("验证码存入redis出错,cause by:%v", err)
+		}
 	}()
-	ctx.JSON(200, result.Success("123456"))
+	ctx.JSON(http.StatusOK, result.Success("123456"))
 }
---
+++ project-user/pkg/dao/redis.go
@@ -0,0 +1,35 @@
+package dao
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
+
+var Rc *RedisCache
+
+type RedisCache struct {
+	rdb *redis.Client
+}
+
+func init() {
+	//初始化连接
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	Rc = &RedisCache{
+		rdb: rdb,
+	}
+}
+
+func (rc *RedisCache) Put(ctx context.Context, key, value string, expire time.Duration) error {
+	err := rc.rdb.Set(ctx, key, value, expire).Err()
+	return err
+}
+
+func (rc *RedisCache) Get(ctx context.Context, key string) (string, error) {
+	result, err := rc.rdb.Get(ctx, key).Result()
+	return result, err
+}
